7960895002bc3ac0db5ef1a162448da3dd804d6f
Ryan Gladstone <ryan.gladstone@gmail.com>
2016-09-07 22:35:58 -0400

    in progress

--- publishconf.py
+++ publishconf.py
@@ -20,5 +20,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-# DISQUS_SITENAME = ""
-# GOOGLE_ANALYTICS = "UA-1324463-15"
+#DISQUS_SITENAME = ""
+#GOOGLE_ANALYTICS = ""
